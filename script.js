6f1cb3be9d2d1aca9713592585d0d8869ae30ddd
Gautam Jinjala <gautamjinjala999@gmail.com>
2024-07-05 19:03:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,63 @@
+// Navbar
+$(document).ready(function () {
+	$("#menu-toggle").click(function () {
+		$("#navbar").toggle();
+		console.log("navbar is show");
+	});
+
+	// auto hide
+	$("a").click(function () {
+		console.log("nabar is hidden");
+		$("#navbar").hide();
+	});
+
+	// Hide navbar
+	window.addEventListener("resize", (e => {
+		var width = $(window).width();
+		if (width > 769) {
+			$("#navbar").hide();
+			console.log("width 768");
+		}
+	}))
+});
+
+// Scrol
+function topfunction() {
+	document.body.scrollTop = 0;
+	document.documentElement.scrollTop = 0;
+}
+
+// Pages
+document.addEventListener('DOMContentLoaded', function () {
+	const links = document.querySelectorAll('.nav-item');
+	const sections = document.querySelectorAll('.division');
+
+	links.forEach(link => {
+		link.addEventListener('click', function (e) {
+			e.preventDefault();
+
+			// Remove active class from all links
+			links.forEach(link => link.classList.remove('active'));
+
+			// Add active class to the clicked link
+			this.classList.add('active');
+
+			// Get the target section id
+			const targetId = this.getAttribute('href').substring(1);
+
+			// Remove active class from all sections
+			sections.forEach(section => section.classList.remove('active'));
+
+			// Add active class to the target section
+			document.getElementById(targetId).classList.add('active');
+
+			const targetSection = document.getElementById(targetId);
+			if (targetSection) {
+				targetSection.classList.add("active");
+
+				// Update URL hash without reloading the page
+				history.pushState(null, null, `#${targetId}`);
+			}
+		});
+	});
+});
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./src/**/*.{html,js}'],
+    theme: {
+        colors: {
+            'primary': '#27749e',
+            'secondary': '#7ece58',
+            'third':'#ffcc00',
+            'four': '#ff6633',
+        },
+        fontFamily: {
+            mochiy: ["Mochiy Pop One", 'sans-serif'],
+        },
+    },
+}
